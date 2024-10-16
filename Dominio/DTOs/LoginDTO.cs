e7a6b2bdf6a0001a68c08003d8cc77d0ed4eb498
Watson Breno <watsonbreno2014@gmail.com>
2024-10-16 15:06:18 -0300

    iniciando projeto

---
+++ Dominio/DTOs/LoginDTO.cs
@@ -0,0 +1,5 @@
+public class LoginDTO
+{
+    public string Email {get; set;}  = default;
+    public string Senha {get; set; } = default;
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System.Runtime.InteropServices;
+using MinimalApi.DTOs;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login aprovado com sucesso");
+    else
+        return Results.Unauthorized();
+
+});
+
+app.Run();
+
